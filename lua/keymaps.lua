8e24e100980fe83527feef2f64ef9f81c11630c3
Razak <moabdulrazak17@gmail.com>
2022-08-21 22:09:57 +0000

    chore(mappings): use global mappings util

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,61 +1,73 @@
+local nnoremap = pvim.nnoremap
+local xnoremap = pvim.xnoremap
+local vnoremap = pvim.vnoremap
+local inoremap = pvim.inoremap
 local opts = { noremap = true, silent = true }
 
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
-
 --Remap space as leader key
 keymap('', '<Space>', '<Nop>', opts)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
--- Normal --
--- Better window navigation
-keymap('n', '<C-h>', '<C-w>h', opts)
-keymap('n', '<C-j>', '<C-w>j', opts)
-keymap('n', '<C-k>', '<C-w>k', opts)
-keymap('n', '<C-l>', '<C-w>l', opts)
-
--- Insert --
--- Press jk fast to enter
-keymap('i', 'jk', '<ESC>', opts)
-
--- Visual --
--- Stay in indent mode
-keymap('v', '<', '<gv', opts)
-keymap('v', '>', '>gv', opts)
-
--- Visual Block --
--- Move text up and down
-keymap('x', 'J', ":move '>+1<CR>gv-gv", opts)
-keymap('x', 'K', ":move '<-2<CR>gv-gv", opts)
-keymap('x', '<A-j>', ":move '>+1<CR>gv-gv", opts)
-keymap('x', '<A-k>', ":move '<-2<CR>gv-gv", opts)
-
--- Undo
-keymap('n', '<C-z>', ':undo<CR>', opts)
-keymap('x', '<C-z>', ':undo<CR><Esc>', opts)
-
--- Disable arrows in normal mode
-keymap('n', '<Up>', '<NOP>', opts)
-keymap('i', '<Up>', '<NOP>', opts)
-keymap('n', '<Down>', '<NOP>', opts)
-keymap('i', '<Down>', '<NOP>', opts)
-keymap('i', '<Left>', '<NOP>', opts)
-keymap('i', '<Right>', '<NOP>', opts)
-
-keymap('n', '<C-s>', ':w!<CR>', { noremap = false })
+keymap('n', '<F5>', ':set relativenumber! number! nocursorline ruler!<CR>', opts)
 keymap('n', '<C-q>', ':q<CR>', { noremap = false })
-keymap('n', '<CR>', 'o<Esc>', opts)
-keymap('n', '<S-Enter>', 'O<Esc>', opts)
-
 keymap('n', '<Left>', ':silent bp<CR> :redraw!<CR>', { noremap = true })
 keymap('n', '<Right>', ':silent bn<CR> :redraw!<CR>', { noremap = true })
 
-keymap('n', '<F5>', ':set relativenumber! number! nocursorline ruler!<CR>', { noremap = false })
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ','
+----------------------------------------------------------------------------------------------------
+-- Window Movement
+----------------------------------------------------------------------------------------------------
+nnoremap('<C-h>', '<C-w>h')
+nnoremap('<C-j>', '<C-w>j')
+nnoremap('<C-k>', '<C-w>k')
+nnoremap('<C-l>', '<C-w>l')
+----------------------------------------------------------------------------------------------------
+-- Press jk fast to enter
+inoremap('jk', '<ESC>')
+----------------------------------------------------------------------------------------------------
+-- Indent
+----------------------------------------------------------------------------------------------------
+vnoremap('<', '<gv')
+vnoremap('>', '>gv')
+----------------------------------------------------------------------------------------------------
+-- Move selected line / block of text in visual mode
+----------------------------------------------------------------------------------------------------
+xnoremap('K', ":m '<-2<CR>gv=gv")
+xnoremap('J', ":m '>+1<CR>gv=gv")
+----------------------------------------------------------------------------------------------------
+-- Line Movement
+----------------------------------------------------------------------------------------------------
+nnoremap('<A-j>', ':m .+1<CR>==')
+nnoremap('<A-k>', ':m .-2<CR>==')
+----------------------------------------------------------------------------------------------------
+-- Undo
+----------------------------------------------------------------------------------------------------
+nnoremap('<C-z>', ':undo<CR>')
+vnoremap('<C-z>', ':undo<CR><Esc>')
+xnoremap('<C-z>', ':undo<CR><Esc>')
+inoremap('<c-z>', [[<Esc>:undo<CR>]])
+----------------------------------------------------------------------------------------------------
+-- Arrows
+----------------------------------------------------------------------------------------------------
+nnoremap('<down>', '<nop>')
+nnoremap('<up>', '<nop>')
+nnoremap('<left>', '<nop>')
+nnoremap('<right>', '<nop>')
+inoremap('<up>', '<nop>')
+inoremap('<down>', '<nop>')
+inoremap('<left>', '<nop>')
+inoremap('<right>', '<nop>')
+----------------------------------------------------------------------------------------------------
+-- Save
+----------------------------------------------------------------------------------------------------
+nnoremap('<C-s>', ':silent! write<CR>')
 
+nnoremap('<CR>', 'o<Esc>')
+nnoremap('<S-Enter>', 'O<Esc>')
 -- Navigate buffers
-keymap('n', '<S-h>', ':bprevious<CR>', opts)
-keymap('n', '<S-l>', ':bnext<CR>', opts)
+nnoremap('H', '<cmd>bprevious<CR>', 'previous buffer')
+nnoremap('L', '<cmd>bnext<CR>', 'next buffer')
 
 vim.cmd([[
 cnoreabbrev W! w!
