e94f27794fad4687b1fe3553aa3f155525e2f1e8
Razak <moabdulrazak17@gmail.com>
2022-11-02 00:29:27 +0000

    chore(spell): enable spell

--- lua/settings.lua
+++ lua/settings.lua
@@ -257,6 +257,7 @@ opt.spellsuggest:prepend({ 12 })
 opt.spelloptions = 'camel'
 opt.spellcapcheck = '' -- don't check for capital letters at start of sentence
 opt.fileformats = { 'unix', 'mac', 'dos' }
+opt.spelllang = 'en'
 opt.spelllang:append('programming')
 opt.spellfile = join_paths(pvim.get_config_dir(), 'spell', 'en.utf-8.add')
 -----------------------------------------------------------------------------//
