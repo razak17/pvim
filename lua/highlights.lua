ec6c6fba416ea4c55eda850a07b0dc5cf38d5960
Razak <moabdulrazak17@gmail.com>
2022-11-04 07:27:09 +0000

    chore(colorscheme): add highlights module

---
+++ lua/highlights.lua
@@ -0,0 +1,5 @@
+local status_ok, _ = pcall(vim.cmd, 'colorscheme zephyr')
+if not status_ok then
+  -- vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -8,6 +8,7 @@ local opts = { noremap = true, silent = true }
 local keymap = vim.api.nvim_set_keymap
 --Remap space as leader key
 keymap('', '<Space>', '<Nop>', opts)
+
 keymap('n', '<F5>', ':set relativenumber! number! nocursorline ruler!<CR>', opts)
 keymap('n', '<C-q>', ':q<CR>', { noremap = false })
 keymap('n', '<Left>', ':silent bp<CR> :redraw!<CR>', { noremap = true })
