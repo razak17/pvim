9d8e401db6401b9e38895ba3dd29b845160deee4
Razak <moabdulrazak17@gmail.com>
2022-11-14 01:35:50 +0000

    chore(cmp-dictionary): update method should be run manually
    every time dict file is modified

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -78,16 +78,12 @@ return packer.startup(function(use)
         'uga-rosa/cmp-dictionary',
         after = 'nvim-cmp',
         config = function()
-          -- Refer to install script
-          local dicwords = join_paths(pvim.get_runtime_dir(), 'site', 'spell', 'dictionary.txt')
-          if vim.fn.filereadable(dicwords) ~= 1 then dicwords = '/usr/share/dict/words' end
           require('cmp_dictionary').setup({
             async = true,
             dic = {
-              ['*'] = dicwords,
+              ['*'] = join_paths(pvim.get_runtime_dir(), 'site', 'spell', 'dictionary.txt'),
             },
           })
-          require('cmp_dictionary').update()
         end,
       },
     },
